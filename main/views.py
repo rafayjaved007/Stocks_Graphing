9dc4fec37e6bc4fc08f671f0f339ffedaa4465f8
rafayjaved007 <61459779+rafayjaved007@users.noreply.github.com>
2021-07-06 15:46:10 +0500

    Update views.py

--- main/views.py
+++ main/views.py
@@ -14,14 +14,22 @@ class HomeView(View):
 
 
 def get_data(request, *args, **kwargs):
+    #
+    # labels = []
+    # while interval <= 5400:
+    #     labels.append(interval)
+    #     interval += 5
     labels = ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60', '65', '70', '75', '80',
               '85', '90']
     chartLabel = "Delta"
     stocks = Stocks.objects.all()
     delta = []
-    for stock in stocks[len(stocks) - 19:]:
-        print(stock.us_tech_100, stock.us_500)
-        delta.append(stock.us_tech_100 - stock.us_500)
+    interval = 0
+    while interval <= 1081:
+        delta.append(stocks[interval].us_tech_100 - stocks[interval].us_500)
+        interval += 60
+
+
 
     data = {
         "labels": labels,
@@ -39,16 +47,31 @@ class SecondGraph(APIView):
     def get(self, request, format=None, time=0):
         labels = ['0', '5', '10', '15', '20', '25', '30', '35', '40', '45', '50', '55', '60', '65', '70', '75', '80',
                   '85', '90']
+        # interval = 0
+        # labels = []
+        # while interval <= 5400:
+        #     labels.append(interval)
+        #     interval += 5
+
         chartLabel = "m Delta"
         stocks = Stocks.objects.all()
         delta = []
-
-        for stock in stocks[len(stocks)-19:]:
-            delta_now = stock.us_tech_100 - stock.us_500
+        interval = 0
+        while interval <= 1081:
+            delta.append(stocks[interval].us_tech_100 - stocks[interval].us_500)
+            delta_now = stocks[interval].us_tech_100 - stocks[interval].us_500
             delta_ago = stocks[int(time / 5)].us_tech_100 - stocks[int(time / 5)].us_500
             slope = (delta_now - delta_ago) / time
             print(delta_now, delta_ago, slope)
             delta.append(slope)
+            interval += 60
+
+        # for stock in stocks[len(stocks)-1081:]:
+        #     delta_now = stock.us_tech_100 - stock.us_500
+        #     delta_ago = stocks[int(time / 5)].us_tech_100 - stocks[int(time / 5)].us_500
+        #     slope = (delta_now - delta_ago) / time
+        #     print(delta_now, delta_ago, slope)
+        #     delta.append(slope)
 
         data = {
             "labels": labels,
